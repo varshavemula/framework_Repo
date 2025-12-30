284e09156f34f9844c7a22dd7182c0ba265d60ca
varsh <varsh@LAPTOP-T1BG6N1Q>
2025-12-30 13:30:29 +0530

    initial commit

---
+++ Selenium_Project/src/test/java/Pac1/OrgTest.java
@@ -0,0 +1,26 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		String url=System.getProperty("url");
+		String browser=System.getProperty("browser");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+
+	   System.out.println("create org test");
+	}
+	@Test
+	public void modifyOrgTest()
+	{
+	   System.out.println("modify org test");
+	}
+}
