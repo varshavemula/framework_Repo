056981805757eaad23d0edaebffa46ea11de8e1e
varsh <varsh@LAPTOP-T1BG6N1Q>
2025-12-30 15:53:31 +0530

    added runtime parameters

--- Selenium_Project/src/test/java/pac2/ContactTest.java
+++ Selenium_Project/src/test/java/pac2/ContactTest.java
@@ -7,7 +7,15 @@ public class ContactTest {
 	@Test
 	public void createContactTest()
 	{
-	   System.out.println("create contact test");
+	    String url=System.getProperty("url");
+		String browser=System.getProperty("browser");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("create contact test");
 	}
 	@Test
 	public void modifyContactTest()
